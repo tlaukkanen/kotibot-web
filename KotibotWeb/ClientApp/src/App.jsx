dcb76467c9bf876ba7dc312a84497c271139dbc6
Tommi Laukkanen <tlaukkanen@users.noreply.github.com>
2022-04-24 14:45:03 +0300

    Updated packages

--- KotibotWeb/ClientApp/src/App.jsx
+++ KotibotWeb/ClientApp/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ThemeProvider, createTheme } from '@material-ui/core/styles'
+import { createTheme, ThemeProvider } from '@material-ui/core'
 import Layout from './components/Layout'
 import Home from './components/Home'
 
