a6ed12a4b5421d2371e188cb5e98586abfc87395
Tommi Laukkanen <tlaukkanen@users.noreply.github.com>
2022-05-08 11:07:04 +0300

    ServiceWorker and manifest tweaks

--- KotibotWeb/ClientApp/src/index.jsx
+++ KotibotWeb/ClientApp/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
 const rootElement = document.getElementById('root')
 
@@ -8,3 +9,5 @@ ReactDOM.render(
   <App />,
   rootElement,
 )
+
+registerServiceWorker()
